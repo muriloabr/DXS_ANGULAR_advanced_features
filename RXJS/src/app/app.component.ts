98086862154bb41d0e075f8b078a82ef2bc0c2ad
Murilo Abreu <murilo_abr@yahoo.com.br>
2022-02-17 15:01:59 -0300

    1.0.0

--- RXJS/src/app/app.component.ts
+++ RXJS/src/app/app.component.ts
@@ -1,30 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subscriber } from 'rxjs';
+import { Observable } from 'rxjs';
 
-@Component({
+@Component({ 
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <div style="text-align:center" class="content">
       <h1>
-        Welcome to {{title}}!
+       {{title}}!
       </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+      <span style="display: block">Resultados serão apresentados no console do navegador</span>
+      <img src="https://dataxstudios.com.br/assets/images/logo_DXS_400_190.png" alt="dxs" width="300"/> 
     </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
   `,
   styles: []
 })
@@ -32,39 +20,115 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     //PROMISE FAZ A REQUISIÇÃO, RETORNA SUCESO OU ERRO E FINALIZA, NECESSITANDO DE RECHAMAR PARA FAZER DE NOVO SE PRECISAR
-    //this.minhaPromise("angular").then(result => console.log(result));
-    //this.minhaPromise("csharp").then(result => console.log(result)).catch(erro => console.log(erro)); //PROMISES PODEM RETORNAR ERRO ENTAO TRATA-SE COM CATCH
+    /*
+    this.minhaPromise("angular")
+      .then(resultadodissoaqui => console.log(resultadodissoaqui)
+    );
+    */
+    /*
+    this.minhaPromise("csharp")
+      .then(resultadodissoaqui => console.log(resultadodissoaqui))
+      .catch(resultadodeerrodissoaqui => console.log(resultadodeerrodissoaqui)
+    ); //PROMISES PODEM RETORNAR ERRO ENTAO TRATA-SE COM CATCH
+    */
+
     //OBSERVABLES FAZEM REQUISIÇÕES SEM PRECISAR FINALIZAR, CRIANDO UMA ASSINATURA DE CONTEUDO, NECESSITANDO CANCELAR ASSINATURAS MANUALMENTE
-    this.minhaObservable("angular").subscribe(result => console.log(result));
-    this.minhaObservable("chsarp").subscribe({next: (result) => console.log(result), error: (erro) => console.log("Erro: " + erro)}); //PROMISES PODEM RETORNAR ERRO ENTAO TRATA-SE COM CATCH
+    /*
+    this.minhaObservable("angular")
+      .subscribe(resultadodissoaqui => console.log(resultadodissoaqui));
+    */
+    /*
+    this.minhaObservable("chsarp")
+      .subscribe( {
+          next: (resultadodissoaqui) => console.log(resultadodissoaqui), //OBSERVABLES RETORNAM RESULTADOS ENTAO CAPTURA ASSIM
+          error: (resultadodeerrodissoaqui) => console.log("Erro: " + resultadodeerrodissoaqui), //OBSERVABLES PODEM RETORNAR ERRO ENTAO TRATA-SE ASSIM
+          complete: () => console.log("Completou essa Observable!!") //OBSERVABLES ENVIAM SINAL DE TAREFA COMPLETADA, RECEBA E TOME ALGUMA ATITUDE
+      }
+    ); 
+    */
+    
+    const observadordissoaqui = { //OBSERVADOR PRONTO COM TODOAS OS PROCEDIMENTOS PARA CADA ESTÁGIO
+      next: (valor: any) => console.log("NEXT: ", valor), 
+      error: (erro: any) => console.log("ERROR: ", erro), //AO DETECTAR UM ERRO DA OBSERVABLE, PARAMOS DE RECEBER DADOS
+      complete: () => console.log("COMPLETE!") //AO RECEBER UM COMPLETE HAVERA DESASSINATURA AUTOMATICA
+    }
+    /*
+    const obs =
+      this.minhaObservable("angular")
+      .subscribe(observadordissoaqui);
+    */
+    const obsUsuario =
+      this.minhaObservableUsuario("admin", "emaul@.com");
+    const subsUsuario = obsUsuario.subscribe(observadordissoaqui);
+
+    setTimeout(() => {
+      subsUsuario.add( //ADD COLOCA ROTINAS PARA QUANDO ESTE OBSERVABLE TIVER O UNSUBSCRIBE() CHAMADO 
+        ()=>console.log("Estamos assim: ", subsUsuario.closed?'fechada!':'aberta!')
+      ); 
+      subsUsuario.unsubscribe(); //DESASSINAR O RECEBIMENTO DE DADOS DE UMA OBSERVABLE      
+    }, 4600);
   }
-  title = 'RXJS';
+  title = 'RXJS | PROMISSES vs OBSERVABLES';
 
   minhaPromise(nome: string): Promise<string> { //METODO RETONA PROMISE()
-    return new Promise((resolve, reject) => {
+    return new Promise((resolverissoaqui, rejectadordissaqui) => {
       if(nome == "angular"){
         setTimeout(() => {
-          resolve("Te conheco: " + nome)
+          resolverissoaqui("Te conheco: " + nome)
+          
         }, 1000);
       } else {
         setTimeout(() => {
-          reject("Voce não tem nome conhecido: " + nome);
+          rejectadordissaqui("Voce não tem nome conhecido: " + nome);
         }, 2000);        
       }
     })
   }
 
   minhaObservable(nome: string): Observable<string> {
-    return new Observable(subscriber => {
+    return new Observable(assinatura => {
       if(nome === "angular"){
-        subscriber.next("Conheço sim: " + nome);
+        assinatura.next("Conheço sim: " + nome); //ENVIO DE DADOS CAPTADOS NESTA ETAPA
+      } else if(nome === "csharp") {
+        assinatura.error("Esse: " + nome + " está com arte!"); //ENVIO DE ERRO
       } else {
-        subscriber.error("Não conheço esse nome ai: " + nome);
+        assinatura.error("Não é do Angular, então esse: " + nome + " está com arte também!"); //ENVIO DE ERRO
       }
-      subscriber.next("Sou sua Observable!!"); //ASSINATURA PARA RECEBER DADOS
+      assinatura.next("Sou sua Observable!!"); //ASSINATURA PARA RECEBER DADOS
       setTimeout(() => {
-        subscriber.next("Sou sua Observable com delay!!"); 
-      }, 2000); 
+        assinatura.next("Sou sua Observable com delay!!"); //ENVIO DE DADOS CAPTADOS NESTA ETAPA
+        assinatura.complete(); //ENVIO DE STATUS QUE COMPLETOU TUDO!
+      }, 2000);      
     });
+  }
+
+  minhaObservableUsuario(nome: string, email: string): Observable<Usuario> {
+    return new Observable(assinatura => {
+      if(nome === "admin") {
+        let usuario = new Usuario(nome, email);
+        setTimeout(() => {
+          assinatura.next(usuario);
+        }, 1500); 
+        setTimeout(() => {
+          assinatura.next(usuario);
+        }, 4500); 
+        setTimeout(() => {
+          assinatura.complete();
+        }, 7000); 
+      } else {
+        assinatura.error("Não é admin!");
+      }
+    });
+  }
+}
+
+export class Usuario {
+
+  nome: string;
+  email: string;
+
+  constructor(nome: string, email: string) {    
+    this.nome = nome;
+    this.email = email;
   }
 }
